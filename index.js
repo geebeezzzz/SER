b3f5a74ece22480758780f8c6ea9d799b6ee2b49
gb <30723719+geebeezzzz@users.noreply.github.com>
2018-11-21 10:48:06 +0100

    index js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ lottie.loadAnimation({
   renderer: 'svg',
   loop: true,
   autoplay: true,
-  path: 'data.json' // the path to the animation json
+  path: 'scripts/data.json' // the path to the animation json
 });
